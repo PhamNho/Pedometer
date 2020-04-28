81f0d16f406ccb29f36195c792fec332ac64566a
DatUET <17020677@vnu.edu.vn>
2020-04-28 14:42:40 +0700

    Initial commit

---
+++ app/src/main/java/com/nhopv/pedometer1/MainActivity.java
@@ -0,0 +1,153 @@
+package com.nhopv.pedometer1;
+
+import android.annotation.SuppressLint;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.mikhaellopez.circularprogressbar.CircularProgressBar;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "NNN";
+    private EditText edtHeight;
+    private EditText edtWeight;
+    private EditText edtTarget;
+    private CircularProgressBar circularProgressBar;
+    private TextView tvSteps;
+    private TextView tvProgress;
+    private TextView tvCalories;
+    private TextView tvKilometers;
+    private Float totalSteps = 0f;
+    private Float previousTotalSteps = 0f;
+    private int steps;
+    private double calo, km;
+
+    public static final String mBroadcastAction = "STRING_BROADCAST_ACTION";
+    private IntentFilter mIntentFilter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initView();
+//        resetSteps();
+//        loadData();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        registerReceiver(mReceiver, mIntentFilter);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unregisterReceiver(mReceiver);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    //Tạo một BroadcastReceiver lắng nghe service
+    private BroadcastReceiver mReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (intent.getAction().equals(mBroadcastAction)) {
+                Log.d(TAG, "steps: " + steps + "\n" + "calo :" + calo + "\n" + "km: " + km);
+                steps = intent.getIntExtra("steps", 0);
+                calo = intent.getDoubleExtra("calo", 0);
+                km = intent.getDoubleExtra("km", 0);
+                totalSteps = intent.getFloatExtra("totalSteps", 0);
+
+                tvSteps.setText(String.valueOf(steps));
+                tvCalories.setText(String.valueOf(calo));
+                tvKilometers.setText(String.valueOf(km));
+                circularProgressBar.setProgressWithAnimation(steps);
+            }
+        }
+    };
+
+    private void initView() {
+        edtHeight = findViewById(R.id.edtHeight);
+        edtWeight = findViewById(R.id.edtWeight);
+        edtTarget = findViewById(R.id.edtTarget);
+        circularProgressBar = findViewById(R.id.circularProgressBar);
+        tvSteps = findViewById(R.id.tvSteps);
+        tvProgress = findViewById(R.id.tvProgress);
+        tvCalories = findViewById(R.id.tvCalories);
+        tvKilometers = findViewById(R.id.tvKilometers);
+        mIntentFilter = new IntentFilter(mBroadcastAction);
+
+    }
+
+    @SuppressLint("SetTextI18n")
+    public void startStepCounter(View view) {
+        String mHeight = edtHeight.getText().toString().trim();
+        String mWeight = edtWeight.getText().toString().trim();
+        String mTarget = edtTarget.getText().toString().trim();
+
+        if (mHeight.isEmpty()) {
+            edtHeight.setError("Bạn chưa nhập chiều cao (cm)");
+        } else if (mWeight.isEmpty()) {
+            edtWeight.setError("Bạn chưa nhập cân nặng (kg)");
+        } else if (mTarget.isEmpty()) {
+            edtTarget.setError("Bạn chưa nhập mục tiêu (bước)");
+        } else {
+            tvProgress.setText("/" + mTarget);
+            Intent intent = new Intent(MainActivity.this, PedometerService.class);
+            intent.putExtra("height", Double.parseDouble(mHeight));
+            intent.putExtra("weight", Double.parseDouble(mWeight));
+            mIntentFilter.addAction(mBroadcastAction);
+            startService(intent);
+        }
+    }
+
+//    void resetSteps() {
+//        tvSteps.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Toast.makeText(MainActivity.this, "Giữ để làm mới", Toast.LENGTH_SHORT).show();
+//            }
+//        });
+//        tvSteps.setOnLongClickListener(new View.OnLongClickListener() {
+//            @Override
+//            public boolean onLongClick(View v) {
+//                previousTotalSteps = totalSteps;
+//                tvSteps.setText(String.valueOf(0));
+//                tvCalories.setText(String.valueOf(0));
+//                tvKilometers.setText(String.valueOf(0));
+//                circularProgressBar.setProgressWithAnimation(0);
+//                saveDate();
+//                Toast.makeText(MainActivity.this, "Đã làm mới", Toast.LENGTH_SHORT).show();
+//                return false;
+//            }
+//        });
+//    }
+
+//    private void saveDate() {
+//        SharedPreferences sharedPreferences = getSharedPreferences("myPrefs", Context.MODE_PRIVATE);
+//        SharedPreferences.Editor editor = sharedPreferences.edit();
+//        editor.putFloat("key1", previousTotalSteps);
+//        editor.apply();
+//    }
+
+//    private void loadData() {
+//        SharedPreferences sharedPreferences = getSharedPreferences("myPrefs", Context.MODE_PRIVATE);
+//        Float saveNumber = sharedPreferences.getFloat("key1", 0f);
+//        Log.d(TAG, String.valueOf(saveNumber));
+//        previousTotalSteps = saveNumber;
+//    }
+}
---
+++ app/src/main/java/com/nhopv/pedometer1/PedometerService.java
@@ -0,0 +1,114 @@
+package com.nhopv.pedometer1;
+
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.IBinder;
+import android.util.Log;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class PedometerService extends Service implements SensorEventListener {
+    private static final String TAG = "AAA";
+    SensorManager sensorManager;
+
+    private Boolean isRunning = false;
+    private Float previousTotalSteps = 0f;
+    int currentSteps;
+    private double height;
+    private double weight;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        Sensor stepSensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
+        if (stepSensor == null) {
+            Toast.makeText(this, "Không có cảm biến được phát hiện trên thiết bị này", Toast.LENGTH_SHORT).show();
+        } else {
+            sensorManager.registerListener(this, stepSensor, SensorManager.SENSOR_DELAY_UI);
+        }
+        loadData();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.d(TAG, "onStartCommand: ");
+        isRunning = true;
+        height = intent.getDoubleExtra("height", 0);
+        weight = intent.getDoubleExtra("weight", 0);
+
+        //flag này có tác dụng khi android bị kill hoặc bộ nhớ thấp, hệ thống sẽ start lại và gửi kết quả lần nữa.
+        return START_REDELIVER_INTENT;
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        Log.d(TAG, "onBind: ");
+        return null;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy: ");
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (isRunning) {
+            Float totalSteps = event.values[0];
+            currentSteps = (int) (totalSteps - previousTotalSteps);
+
+            double averageSpeed = 0.0008; // km/bước
+            double calorieConsumption = Math.ceil(currentSteps * CountCaloriesFor1Step() * 100) / 100;
+            double kmReach = Math.ceil(currentSteps * averageSpeed * 100) / 100;
+            Log.d(TAG, "onSensorChanged: " + currentSteps);
+
+            Intent broadcastIntent = new Intent();
+            broadcastIntent.setAction(MainActivity.mBroadcastAction);
+            broadcastIntent.putExtra("steps", currentSteps);
+            broadcastIntent.putExtra("calo", calorieConsumption);
+            broadcastIntent.putExtra("km", kmReach);
+            broadcastIntent.putExtra("totalSteps", totalSteps);
+            sendBroadcast(broadcastIntent);
+        }
+    }
+
+    private void saveDate() {
+        SharedPreferences sharedPreferences = getSharedPreferences("myPrefs", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putFloat("key1", previousTotalSteps);
+        editor.apply();
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+    }
+
+    private void loadData() {
+        SharedPreferences sharedPreferences = getSharedPreferences("myPrefs", Context.MODE_PRIVATE);
+        Float saveNumber = sharedPreferences.getFloat("key1", 0f);
+        Log.d(TAG, String.valueOf(saveNumber));
+        previousTotalSteps = saveNumber;
+    }
+
+    private double CountCaloriesFor1Step() {
+        double Calories;
+        double averageSpeed = 5;
+        double caloriesFor1Minute = (0.035 * weight) + ((averageSpeed * averageSpeed) / height) * 0.029 * weight * 100;
+        Calories = caloriesFor1Minute / 100;
+        return Calories;
+    }
+}
